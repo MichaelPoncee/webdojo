1ef60ba09fc1352182689cbad33012046c5e1268
michael ponce <michaelponceeferreira@gmail.com>
2025-09-24 09:58:31 -0300

    Criação do Mock da api de cadastro

--- api/index.js
+++ api/index.js
@@ -1,14 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3333
+
+//habilita CORS para todas as origens, para a aplicação rodar a api
+app.use(cors())//se fosse colocar em prod, teria que colocar o endereço na api
+
+//trabalhar com envio de json
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'API do curso Ninja do Cypress!' })
 })
-
+//criando o retorno de sucesso quando criar um usuario
 app.post('/api/users/register', (req, res) => {
 
-  return res.status(201).end()
+  // criando constantes para o json
+  const { name, email, password } = req.body
+
+  // se o nome ficar em branco ou campo deletato = mensagem de erro Name is required o (!name) obriga ele ser um campo obrigatorio
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' })
+  }
+
+  // se o email ficar em branco ou campo deletato = mensagem de erro Email is required o (!email) obriga ele ser um campo obrigatorio
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' })
+  }
+
+  // se o password ficar em branco ou campo deletato = mensagem de erro password is required o (!password) obriga ele ser um campo obrigatorio
+  if (!password) {
+    return res.status(400).json({ error: 'Password is required' })
+  }
+
+  //imprimir o corpo da req
+  console.log(req.body)
+  return res.status(201).json({ message: 'Usuário cadastrado com sucesso!' })
 
 })
 
--- web/cypress/e2e/signup.cy.js
+++ web/cypress/e2e/signup.cy.js
@@ -3,13 +3,14 @@ describe('Cadastro', () => {
         //commands criado para acessar o sistema e clicar no registre-se: linha 37 a 43 de commands
         cy.gotoSignup()
         //Forçar o sistema apresentar a mensagem de sucesso ao invés de chamar a API real "MOCK, QUE ESTÁ REALMENTE EXECUTANDO O BOTÃO E ESTÁ TENDO UMA AÇÃO"
-        cy.intercept('POST','http://localhost:3333/api/users/register',
+        
+        /*cy.intercept('POST','http://localhost:3333/api/users/register',
         {
             statusCode: 201,
             body: {
                 message:'usuário cadastrado com sucesso'
             }
-        }).as('postSignup') // até aqui 
+        }).as('postSignup') // até aqui */
     })
 
     it('Deve cadastrar um novo usuário', () => {
@@ -22,7 +23,7 @@ describe('Cadastro', () => {
         cy.contains('button', 'Criar conta')
             .click()
 
-            cy.wait('@postSignup') // mostrando a mensagem do mock que fiz com a mensagem de sucesso , linha 5 a linha 12
+            //cy.wait('@postSignup') // mostrando a mensagem do mock que fiz com a mensagem de sucesso , linha 5 a linha 12
             cy.contains('Conta criada com sucesso!')
             .should('be.visible')
         })
